ea6b805ce20f4d8ba061af256e318ac672ee3742
amitwalke <walkeamit252@gmail.com>
2019-01-23 20:35:40 +0530

    GET API WITH DATA DONE

--- app/src/main/java/app/com/mapdemo/MapsActivity.java
+++ app/src/main/java/app/com/mapdemo/MapsActivity.java
@@ -1,17 +1,14 @@
 package app.com.mapdemo;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,12 +22,10 @@ import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterItem;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
-import com.google.maps.android.ui.IconGenerator;
 
 import java.util.List;
 
 import app.com.mapdemo.model.MarkerItem;
-import app.com.mapdemo.model.MarkerItemParent;
 import app.com.mapdemo.network.ApiClient;
 import app.com.mapdemo.network.ApiInterface;
 import retrofit2.Call;
@@ -52,7 +47,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        fetMarkerList();
     }
 
 
@@ -62,7 +56,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setRotateGesturesEnabled(false);
         mClusterManager = new ClusterManager<>(this, mMap);
         mMap.setOnMarkerClickListener(mClusterManager);
-
 
         final CustomClusterRenderer renderer = new CustomClusterRenderer(this, mMap, mClusterManager);
 
@@ -75,70 +68,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.setOnCameraChangeListener(mClusterManager);
 
+        fetMarkerList();
 
-        for (int i = 0; i < 10; i++) {
-            final LatLng latLng = new LatLng(18.5700755 + i, 73.7298294 + i);
-            mClusterManager.addItem(new StringClusterItem("Hotel #" + (i + 1), latLng));
-        }
-
-
-        final LatLng latLng = new LatLng(18.5701755, 73.7208294);
-        mClusterManager.addItem(new StringClusterItem("Jagdamb #", latLng));
-
-        final LatLng latLng1 = new LatLng(18.5599707, 73.7663066);
-        mClusterManager.addItem(new StringClusterItem("Hybd Biryani #", latLng1));
-
-
-        final LatLng latLng2 = new LatLng(18.5607418, 73.7857914);
-        mClusterManager.addItem(new StringClusterItem("Bottoms Up #", latLng2));
-
-
-        final LatLng latLng3 = new LatLng(18.5483957, 73.7728376);
-        mClusterManager.addItem(new StringClusterItem("BlueRidge #", latLng3));
-
-
-        final LatLng latLng4 = new LatLng(18.5263219, 73.7621671);
-        mClusterManager.addItem(new StringClusterItem("Yawele #", latLng4));
-
-
-        LatLng pune = new LatLng(18.5263219, 73.7621671);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
-
+//        LatLng pune = new LatLng(18.5263219, 73.7621671);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(pune));
 
 
         mClusterManager.setOnClusterClickListener(
                 new ClusterManager.OnClusterClickListener<StringClusterItem>() {
-                    @Override public boolean onClusterClick(Cluster<StringClusterItem> cluster) {
-
+                    @Override
+                    public boolean onClusterClick(Cluster<StringClusterItem> cluster) {
                         Toast.makeText(MapsActivity.this, "Cluster click", Toast.LENGTH_SHORT).show();
-
-                        // if true, do not move camera
-
                         return false;
                     }
                 });
 
         mClusterManager.setOnClusterItemClickListener(
                 new ClusterManager.OnClusterItemClickListener<StringClusterItem>() {
-                    @Override public boolean onClusterItemClick(StringClusterItem clusterItem) {
-
+                    @Override
+                    public boolean onClusterItemClick(StringClusterItem clusterItem) {
                         Toast.makeText(MapsActivity.this, "Cluster item click", Toast.LENGTH_SHORT).show();
-
-                        // if true, click handling stops here and do not show info view, do not move camera
-                        // you can avoid this by calling:
-                        // renderer.getMarker(clusterItem).showInfoWindow();
-
                         return false;
                     }
                 });
-
-
-
     }
 
 
     public class CustomClusterRenderer extends DefaultClusterRenderer<StringClusterItem> {
-
         public CustomClusterRenderer(Context context, GoogleMap map,
                                      ClusterManager<MapsActivity.StringClusterItem> clusterManager) {
             super(context, map, clusterManager);
@@ -149,10 +105,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                                    MarkerOptions markerOptions) {
             final BitmapDescriptor markerDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.map_location_icon);
             markerOptions.icon(markerDescriptor).snippet(item.title);
-
             markerOptions.icon(markerDescriptor);
         }
-
 
         @Override
         protected void onBeforeClusterRendered(Cluster<StringClusterItem> cluster, MarkerOptions markerOptions) {
@@ -167,9 +121,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final String title;
         final LatLng latLng;
 
-        public StringClusterItem(String title, LatLng latLng) {
-            this.title = title;
-            this.latLng = latLng;
+        public StringClusterItem(MarkerItem markerItem) {
+            this.title = markerItem.getName();
+            this.latLng = new LatLng(Double.parseDouble(markerItem.getLat()), Double.parseDouble(markerItem.getLon()));
         }
 
         @Override
@@ -211,6 +165,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onResponse(Call<List<MarkerItem>> call, Response<List<MarkerItem>> response) {
                 if (response.isSuccessful()) {
                     Log.i(TAG, "onResponse: " + response.body().size());
+                    if (response != null && response.body() != null && response.body().size() != 0) {
+                        LatLng latLng = new LatLng(Double.parseDouble(response.body().get(0).getLat()),
+                                Double.parseDouble(response.body().get(0).getLon()));
+                        for (MarkerItem item : response.body()) {
+                            mClusterManager.addItem(new StringClusterItem(item));
+                        }
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    }
                 }
             }
 
@@ -223,3 +185,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+  /*for (int i = 0; i < 10; i++) {
+            final LatLng latLng = new LatLng(18.5700755 + i, 73.7298294 + i);
+            mClusterManager.addItem(new StringClusterItem("Hotel #" + (i + 1), latLng));
+        }
+
+
+        final LatLng latLng = new LatLng(18.5701755, 73.7208294);
+        mClusterManager.addItem(new StringClusterItem("Jagdamb #", latLng));
+
+        final LatLng latLng1 = new LatLng(18.5599707, 73.7663066);
+        mClusterManager.addItem(new StringClusterItem("Hybd Biryani #", latLng1));
+
+
+        final LatLng latLng2 = new LatLng(18.5607418, 73.7857914);
+        mClusterManager.addItem(new StringClusterItem("Bottoms Up #", latLng2));
+
+
+        final LatLng latLng3 = new LatLng(18.5483957, 73.7728376);
+        mClusterManager.addItem(new StringClusterItem("BlueRidge #", latLng3));
+
+
+        final LatLng latLng4 = new LatLng(18.5263219, 73.7621671);
+        mClusterManager.addItem(new StringClusterItem("Yawele #", latLng4));*/
